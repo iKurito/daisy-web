12dc62c9bb2d4fb4cd75499cb93621c0528cd7bd
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2023-04-24 22:54:47 -0500

    feat: validate repeats

--- src/pages/Search/components/ProcessResult.tsx
+++ src/pages/Search/components/ProcessResult.tsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-nested-ternary */
 import { useSearchContext } from '../context/search.context';
 import { tabs } from '../data';
 
 function ProcessResult() {
   const { response, activeTab, handleSetTab } = useSearchContext();
-
   const size = Object.entries(response).length;
+  const items =
+    size === 0
+      ? []
+      : response.proteinResult.isRepeat
+      ? tabs
+      : tabs.filter((tab) => tab.id !== 2);
 
   if (size === 0) return null;
 
@@ -17,22 +23,24 @@ function ProcessResult() {
       ) : (
         <>
           <ul className="flex overflow-x-auto test-sm font-semibold text-center gap-1">
-            {tabs.map((tab) => (
-              <li key={tab.id}>
-                <button
-                  type="button"
-                  className={`${
-                    activeTab === tab.id
-                      ? 'bg-primary text-gray-900'
-                      : 'bg-gray-500 text-gray-100'
-                  } flex xs:gap-2 items-center justify-center px-2 py-4 sm:p-4 rounded-t-lg min-w-[150px]`}
-                  onClick={() => handleSetTab(tab.id)}
-                >
-                  <div>{tab.icon}</div>
-                  {tab.name}
-                </button>
-              </li>
-            ))}
+            {items.map((tab) => {
+              return (
+                <li key={tab.id}>
+                  <button
+                    type="button"
+                    className={`${
+                      activeTab === tab.id
+                        ? 'bg-primary text-gray-900'
+                        : 'bg-gray-500 text-gray-100'
+                    } flex xs:gap-2 items-center justify-center px-2 py-4 sm:p-4 rounded-t-lg min-w-[150px]`}
+                    onClick={() => handleSetTab(tab.id)}
+                  >
+                    <div>{tab.icon}</div>
+                    {tab.name}
+                  </button>
+                </li>
+              );
+            })}
           </ul>
           {tabs.find((tab) => tab.id === activeTab)?.component}
         </>
