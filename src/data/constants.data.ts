0e672b19793255aa5b9248abc1cfce1041f8c012
iKurito <ronaldo.tunque@pucp.edu.pe>
2023-06-21 19:24:28 -0500

    feat: add colors

--- src/data/constants.data.ts
+++ src/data/constants.data.ts
@@ -124,24 +124,54 @@ export const classAndSubclassFive: IdNameValue[] = [
 ];
 
 export const unitsColor = [
-  { r: 255, g: 0, b: 0 },       // Rojo
-  { r: 0, g: 0, b: 255 },       // Azul
-  { r: 0, g: 255, b: 0 },       // Verde
-  { r: 255, g: 255, b: 0 },     // Amarillo
-  { r: 255, g: 165, b: 0 },     // Naranja
-  { r: 255, g: 192, b: 203 },   // Rosa
-  { r: 128, g: 0, b: 128 },     // Morado
-  { r: 64, g: 224, b: 208 },    // Turquesa
-  { r: 255, g: 0, b: 255 },     // Magenta
-  { r: 0, g: 255, b: 255 },     // Celeste
-  { r: 165, g: 42, b: 42 },     // Marrón
-  { r: 0, g: 100, b: 0 },       // Verde oscuro
-  { r: 255, g: 215, b: 0 },     // Oro
-  { r: 0, g: 255, b: 255 },     // Cian
-  { r: 230, g: 230, b: 250 },   // Lavanda
-  { r: 245, g: 245, b: 220 },   // Beige
-  { r: 128, g: 128, b: 0 },     // Verde oliva
-  { r: 255, g: 20, b: 147 },    // Rosa chicle
-  { r: 218, g: 165, b: 32 },    // Oro viejo
-  { r: 75, g: 0, b: 130 }       // Índigo
+  { r: 255, g: 0, b: 0 },         // Rojo
+  { r: 0, g: 0, b: 255 },         // Azul
+  { r: 0, g: 255, b: 0 },         // Verde
+  { r: 255, g: 255, b: 0 },       // Amarillo
+  { r: 255, g: 165, b: 0 },       // Naranja
+  { r: 255, g: 192, b: 203 },     // Rosa
+  { r: 128, g: 0, b: 128 },       // Morado
+  { r: 64, g: 224, b: 208 },      // Turquesa
+  { r: 255, g: 0, b: 255 },       // Magenta
+  { r: 165, g: 42, b: 42 },       // Marrón
+  { r: 0, g: 100, b: 0 },         // Verde oscuro
+  { r: 255, g: 215, b: 0 },       // Oro
+  { r: 0, g: 255, b: 255 },       // Cian
+  { r: 230, g: 230, b: 250 },     // Lavanda
+  { r: 245, g: 245, b: 220 },     // Beige
+  { r: 128, g: 128, b: 0 },       // Verde oliva
+  { r: 255, g: 20, b: 147 },      // Rosa chicle
+  { r: 218, g: 165, b: 32 },      // Oro viejo
+  { r: 75, g: 0, b: 130 },        // Índigo
+  { r: 139, g: 0, b: 0 },         // Marrón oscuro
+  { r: 173, g: 255, b: 47 },      // Verde amarillento
+  { r: 30, g: 144, b: 255 },      // Azul real
+  { r: 210, g: 105, b: 30 },      // Chocolate
+  { r: 147, g: 112, b: 219 },     // Azul medio
+  { r: 255, g: 228, b: 196 },     // Almendra
+  { r: 0, g: 139, b: 139 },       // Cian oscuro
+  { r: 220, g: 20, b: 60 },       // Carmesí
+  { r: 255, g: 99, b: 71 },       // Tomate
+  { r: 70, g: 130, b: 180 },      // Acero
+  { r: 0, g: 255, b: 255 },       // Celeste
+  { r: 184, g: 134, b: 11 },      // Oro antiguo
+  { r: 0, g: 128, b: 0 },         // Verde
+  { r: 210, g: 180, b: 140 },     // Marrón claro
+  { r: 135, g: 206, b: 235 },     // Azul cielo
+  { r: 255, g: 165, b: 79 },      // Melocotón
+  { r: 0, g: 191, b: 255 },       // Azul claro
+  { r: 144, g: 238, b: 144 },     // Verde claro
+  { r: 255, g: 140, b: 0 },       // Naranja oscuro
+  { r: 176, g: 224, b: 230 },     // Azul claro pálido
+  { r: 255, g: 192, b: 0 },       // Amarillo oscuro
+  { r: 210, g: 105, b: 30 },      // Chocolate
+  { r: 176, g: 224, b: 230 },     // Azul claro pálido
+  { r: 255, g: 192, b: 0 },       // Amarillo oscuro
+  { r: 60, g: 179, b: 113 },      // Verde mar
+  { r: 139, g: 69, b: 19 },       // Siena
+  { r: 240, g: 230, b: 140 },     // Amarillo claro
+  { r: 128, g: 0, b: 0 },         // Granate
+  { r: 46, g: 139, b: 87 },       // Verde mar oscuro
+  { r: 205, g: 133, b: 63 },      // Burlywood
+  { r: 0, g: 0, b: 128 }          // Azul marino
 ]
